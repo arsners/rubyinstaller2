0d382c6c4bd77888fa0c4da24f97c666d8af2db8
Lars Kanis <lars@greiz-reinsdorf.de>
2017-06-03 18:57:05 +0200

    debug

--- test/test_ridk.rb
+++ test/test_ridk.rb
@@ -52,6 +52,11 @@ module RidkTests
   def test_ridk_version
     skip unless File.directory?("C:/msys64")
 
+    puts "PATH-dirs:"
+    ENV['PATH'].split(";").each{|d| p [d, Dir[d.gsub("\\","/")+"/*"]] }
+    puts "PATH: #{ENV['PATH']}"
+    p run_capture_output("echo %PATH%")
+
     out = run_capture_output "ridk version"
     y = YAML.load(out)
 
